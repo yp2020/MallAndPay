6544b5d4a0de1bf3af447eefa3cf518a64b11015
Wayne <3522373084@qq.com>
2022-01-07 23:31:49 +0800

    mall 系统

---
+++ mall/src/test/java/com/study/mall/MallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
