de1b1b39b5ba3f1f3e4bb458385def81b2390b53
Wayne <3522373084@qq.com>
2022-01-09 19:43:10 +0800

    生成shipping，也就是购物车对象

---
+++ mall/src/main/java/com/study/mall/dao/ShippingMapper.java
@@ -0,0 +1,19 @@
+package com.study.mall.dao;
+
+import com.study.mall.pojo.Shipping;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ShippingMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Shipping record);
+
+    int insertSelective(Shipping record);
+
+    Shipping selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Shipping record);
+
+    int updateByPrimaryKey(Shipping record);
+}
