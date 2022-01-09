a6d62a8be7fc7b1da8a44203dddeaabe71472c93
Wayne <3522373084@qq.com>
2022-01-09 19:41:26 +0800

    生成orderItem 相关对象

---
+++ mall/src/main/java/com/study/mall/dao/OrderItemMapper.java
@@ -0,0 +1,19 @@
+package com.study.mall.dao;
+
+import com.study.mall.pojo.OrderItem;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
