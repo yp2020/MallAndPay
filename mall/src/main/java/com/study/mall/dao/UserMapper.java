2e4aa7b93fd303064cb33e1f65547cbb11c5fb9e
Wayne <3522373084@qq.com>
2022-01-14 21:16:59 +0800

    开始写用户注册模块，这里先生成对应的实体类

---
+++ mall/src/main/java/com/study/mall/dao/UserMapper.java
@@ -0,0 +1,19 @@
+package com.study.mall.dao;
+
+import com.study.mall.pojo.User;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
